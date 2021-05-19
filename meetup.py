a5093227bdfe82df3424d10a0ce563568bc7c08b
elizabethsotomayor <elizabethrsotomayor@gmail.com>
2021-05-18 21:30:31 -0500

    refactor code

--- meetup.py
+++ meetup.py
@@ -20,14 +20,12 @@ def meetup(year: int, month: int, week: str, day_of_week: str):
     for num in range(1, calendar.monthrange(date.year, date.month)[1] + 1):
         # new datetime object for each day in the month
         new_day = datetime.datetime(year, month, num)
-        # print(num)
+
         new_day_of_week = calendar.day_name[new_day.weekday()]
+
         if day_of_week == new_day_of_week:
             occurence += 1
 
-        # print(occurence)
-        # print(new_day_of_week)
-        # print(day_of_week == new_day_of_week)
         if day_of_week == new_day_of_week:
             if week == "1st" and occurence == 1:
                 date_to_return = datetime.date(year, month, num)
@@ -38,13 +36,23 @@ def meetup(year: int, month: int, week: str, day_of_week: str):
             elif week == "3rd" and occurence == 3:
                 date_to_return = datetime.date(year, month, num)
                 break
-            elif week == "teenth" and str(new_day.day)[0] == "1" and occurence >= 2:
+            elif week == "teenth" and str(new_day.day)[0] == "1" and int(str(new_day.day)[1]) > 2:
                 date_to_return = datetime.date(year, month, num)
                 break
             elif week == "4th" and occurence == 4:
                 date_to_return = datetime.date(year, month, num)
                 break
-            elif week == "last" and occurence == 5:
+            elif week == "5th" and occurence == 5:
+                date_to_return = datetime.date(year, month, num)
+                break
+            elif week == "last" and occurence >= 4 and int(str(new_day.day)[0]) >= 2 and int(str(new_day.day)) > 24:
+                date_to_return = datetime.date(year, month, num)
+                break
+            # for February leap years
+            elif week == "last" and occurence >= 4 and new_day.month == 2 and calendar.monthrange(date.year, date.month)[1] > 28:
+                date_to_return = datetime.date(year, month, num)
+            # for normal February years
+            elif week == "last" and occurence >= 4 and new_day.month == 2:
                 date_to_return = datetime.date(year, month, num)
                 break
 
@@ -52,11 +60,4 @@ def meetup(year: int, month: int, week: str, day_of_week: str):
 
 
 def MeetupDayException():
-    return None
-
-
-# meetup(2013, 4, "teenth", "Tuesday")  # date(2013, 4, 16)
-
-# print(meetup(2013, 8, "1st", "Wednesday"))  # date(2013, 8, 7)
-# print(meetup(2013, 6, "teenth", "Wednesday")) # date(2013, 6, 19))
-print(meetup(2013, 2, "teenth", "Wednesday"))  # date(2013, 2, 13))
+    raise TypeError('%s() arg 1 must be %s')
